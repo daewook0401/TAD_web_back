1fc5791748ff4e7779e2bffc84f3624077c41405
daewook0401 <daewook0401@naver.com>
2025-11-15 13:46:26 +0900

    jwt util

--- src/main/java/com/tad/www/configuration/jwt/JwtUtil.java
+++ src/main/java/com/tad/www/configuration/jwt/JwtUtil.java
@@ -1,13 +1,15 @@
 package com.tad.www.configuration.jwt;
 
 import java.util.Base64;
+import java.util.Date;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,7 +23,32 @@ public class JwtUtil {
     @PostConstruct
     public void init(){
         byte[] keyArr = Base64.getDecoder().decode(secretKey);
-        //this.key = Keys.hmacShaKeyFor(keyArr);
+        this.key = Keys.hmacShaKeyFor(keyArr);
     }
-    
+
+    public String getAccessToken(String memberId){
+        return Jwts.builder()
+                    .subject(memberId)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + (3600000L * 24)))
+                    .signWith(key)
+                    .compact();
+    }
+
+    public String getRefreshToken(String memberId){
+        return Jwts.builder()
+                    .subject(memberId)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + (3600000L * 24 * 3)))
+                    .signWith(key)
+                    .compact();
+    }
+    public String getRefreshToken(String memberId, int day){
+        return Jwts.builder()
+                    .subject(memberId)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + (3600000L * 24 * day)))
+                    .signWith(key)
+                    .compact();
+    }
 }
