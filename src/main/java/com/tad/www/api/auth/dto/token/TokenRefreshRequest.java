0444a63a17b11ebdc0dc6365d0f5d5dd00f32f13
Daewook Kim <daewook.kim@vtw.co.kr>
2026-04-10 10:26:04 +0900

    토큰 재발급 처리 수정

--- src/main/java/com/tad/www/api/auth/dto/token/TokenRefreshRequest.java
+++ src/main/java/com/tad/www/api/auth/dto/token/TokenRefreshRequest.java
@@ -1,10 +1,13 @@
 package com.tad.www.api.auth.dto.token;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class TokenRefreshRequest {
+
+    @NotBlank(message = "INVALID_REFRESH_TOKEN")
     private String refreshToken;
 }
